0c2e8eefcd6caafe1cfebb07de8bd1b98c9a1806
Jona Raphael <jona@lumeter.net>
2016-11-15 22:10:34 -0800

    First redis example

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -1,8 +1,27 @@
 from flask import Flask
+import redis
+
 app = Flask(__name__)
+
 @app.route("/")
 def hello():
     return "Hello, I love Digital Ocean!"
+
+@app.route("/database")
+def redisexample():
+    r = redis.Redis('localhost')
+    r.set('key', 'special_value')
+
+    return r.get('key')
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
     app.run()
+
 
