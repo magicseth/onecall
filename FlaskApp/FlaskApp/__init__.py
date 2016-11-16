9a3cc5c95d72c18bec16ccfac2372601d7d202cc
Seth Raphael <seth@bu.mp>
2016-11-15 22:44:19 -0800

    Call jona

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -20,7 +20,9 @@ def redisexample():
 def hello_monkey():
     """Respond to incoming requests."""
     resp = twilio.twiml.Response()
-    resp.say("It's time to call Paul Ryan")
+    resp.say("It's time to call Jona")
+        # Dial (310) 555-1212 - connect that number to the incoming caller.
+        resp.dial("+16178432883")
 
     return str(resp)
 
