cd89ad7f5315a4884f838f15634ffd4b97d55c3f
Jona Raphael <jona@lumeter.net>
2016-11-19 00:01:06 -0800

    Basic Database debugging tools in place

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -1,28 +1,52 @@
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, redirect
+from pyzipcode import ZipCodeDatabase
+import csv
 import redis
 import twilio.twiml
 
 app = Flask(__name__)
+r = redis.Redis('localhost')
+if not r.get('loaded')=='True':
+	with open('./issues.csv') as f:
+		csv_data = csv.reader(f)
+		for row in csv_data:
+			[r.hset(row[1],row[i*2],row[i*2+1]) for i in range(1,len(row)/2)]
+	r.set('loaded', 'True')
+
+@app.route("/")
+def populatelanding():
+	return app.send_static_file('landing.html')
 
 @app.route("/hi")
 def hello():
     return "Hello, I love Digital Ocean!"
 
-@app.route("/database")
-def redisexample():
-    r = redis.Redis('localhost')
-    r.set('key', 'special_value')
+@app.route("/db")
+def dbdump():
+	strings = str([(k, r.get(k)) for k in r.keys() if r.type(k)=='string'])
+	hashes = str([(k, r.hgetall(k)) for k in r.keys() if r.type(k)=='hash'])
+	lists = str([(k, r.lrange(k,0,-1)) for k in r.keys() if r.type(k)=='list'])
+	sets = str([(k, r.smembers(k)) for k in r.keys() if r.type(k)=='set'])
+	return strings+hashes+lists+sets
 
-    return r.get('key')
+@app.route("/flush")
+def flushdb():
+	r.flushall()
+	return redirect('/')
 
-@app.route("/")
-def sendhtmltest():
-	return app.send_static_file('landing.html')
-
-@app.route('/registerNewUser/', methods=['GET', 'POST'])
+@app.route('/registerNewUser', methods=['GET', 'POST'])
 def registerNewUser():
-	print request.form
-
+	zc = request.form.get('zipcode')
+	ph = request.form.get('phonenumber')
+	try:
+		delta = ZipCodeDatabase()[zc].timezone
+	except:
+		delta=0 # This is invalid, as we won't call them on the right timezone
+		# raise UserWarning('Invalid Zip') # do we need to make an actual error class?
+	time = str(int(request.form.get('hour'))-delta)+":"+request.form.get('minute')+" "+request.form.get('ampm') # use the zipcode to change the time to GMT
+	r.hset(ph,'zipcode',zc)
+	r.hset(ph,'calltime',time)
+	return redirect('/')
 
 @app.route("/callpaul", methods=['GET', 'POST'])
 def hello_monkey():
@@ -39,7 +63,6 @@ def hello_monkey():
 
 
 
+
 if __name__ == "__main__":
-    app.run()
-
-
+    app.run(debug=True) # Set debug=True so that saving this file automatically restarts the flask app
