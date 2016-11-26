6c72e4b204a985f61986749667ef40bdf1f4f6f4
Jona Raphael <jona@lumeter.net>
2016-11-26 02:20:43 -0800

    Whitespace

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -25,29 +25,29 @@ auth_token = os.environ['TWILIO_AUTH']
 ### DB Maintenance ###
 
 def connect_db():
-    databasefile = os.path.join(dir_path, 'onecall.sqlt')
-    # The sqlite3.PARSE_DECLTYPES is so that a timestamp column will get parsed correctly.
-    rv = sqlite3.connect(databasefile, detect_types=sqlite3.PARSE_DECLTYPES)
-    rv.execute('pragma foreign_keys = on')
-    # Dont wait for operating system http://www.sqlite.org/pragma.html#pragma_synchronous
-    #http://web.utk.edu/~jplyon/sqlite/SQLite_optimization_FAQ.html#pragma-synchronous
-    rv.execute('pragma synchronous = off')
-    rv.row_factory = sqlite3.Row
-    return rv
+	databasefile = os.path.join(dir_path, 'onecall.sqlt')
+	# The sqlite3.PARSE_DECLTYPES is so that a timestamp column will get parsed correctly.
+	rv = sqlite3.connect(databasefile, detect_types=sqlite3.PARSE_DECLTYPES)
+	rv.execute('pragma foreign_keys = on')
+	# Dont wait for operating system http://www.sqlite.org/pragma.html#pragma_synchronous
+	#http://web.utk.edu/~jplyon/sqlite/SQLite_optimization_FAQ.html#pragma-synchronous
+	rv.execute('pragma synchronous = off')
+	rv.row_factory = sqlite3.Row
+	return rv
 
 def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
+	"""Opens a new database connection if there is none yet for the
+	current application context.
+	"""
+	if not hasattr(g, 'sqlite_db'):
+		g.sqlite_db = connect_db()
+	return g.sqlite_db
 
 def init_db():
-    db = get_db()
-    with app.open_resource('db.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
+	db = get_db()
+	with app.open_resource('db.sql', mode='r') as f:
+		db.cursor().executescript(f.read())
+	db.commit()
 
 def populateTestDB():
 	insertR('caller',['1000000000', '94107', '2016-11-26 13:00:00', 1])
@@ -65,102 +65,101 @@ def populateTestDB():
 
 @app.cli.command('initdb')
 def initdb_command():
-    """Initializes the database."""
-    init_db()
-    print 'Initialized the database.'
+	"""Initializes the database."""
+	init_db()
+	print 'Initialized the database.'
+	populateTestDB()
 
 @app.teardown_appcontext
 def close_db(error):
-    """Closes the database again at the end of the request."""
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
+	"""Closes the database again at the end of the request."""
+	if hasattr(g, 'sqlite_db'):
+		g.sqlite_db.close()
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ### URL Calls ###
 def insertR(table, r, update=False):
-    """
-    Standard insert method that uses the insertstr defined in each class
-    call this from iinsert(..<class dependent field list>.) in each class
-    Note - can pass record as parameters and will auto-convert to id.
-    """
-    insertstr = {
-        "caller":"INSERT INTO caller VALUES (NULL,?,?,?,?)",
-        "campaign": "INSERT INTO campaign VALUES (NULL,?,?,?,?,?)",
-        "target": "INSERT INTO target VALUES (NULL,?,?,?,?)",
-        "region": "INSERT INTO region VALUES (NULL,?,?)",
-        "call": "INSERT INTO call VALUES (NULL,?,?,?)",
-    }
-    if update and table=='caller':
-    	c = find(table, ONEORNONE, phone=r[0])
-    	if c:
-    		idUpdateFields(table, c[0], phone=r[0], zipcode=r[1], calltime=r[2], active=r[3])
-    		return c[0] # Return the ID of the object
+	"""
+	Standard insert method that uses the insertstr defined in each class
+	call this from iinsert(..<class dependent field list>.) in each class
+	Note - can pass record as parameters and will auto-convert to id.
+	update = True If we want to write over existing entries rather than create a new one. Note that there is a uniqueness constraint on the caller.phone.
+	"""
+	insertstr = {
+		"caller":"INSERT INTO caller VALUES (NULL,?,?,?,?)",
+		"campaign": "INSERT INTO campaign VALUES (NULL,?,?,?,?,?,?)",
+		"call": "INSERT INTO call VALUES (NULL,?,?,?,?,?,?)",
+	}
+	if update and table=='caller': 
+		c = find(table, ONEORNONE, phone=r[0])
+		if c:
+			idUpdateFields(table, c['id'], phone=r[0], zipcode=r[1], calltime=r[2], active=r[3])
+			return c['id'] # Return the ID of the object
 	curs = sqlSend(insertstr[table], r)  # Can throw sqlite3.IntegrityError if doesnt match constraint in table structure
 	return curs.lastrowid
 
 def idUpdateFields(table, id, _skipNone=False, **kwargs):
-    """
-    Update Class[id][field]= newvalue for all (field, newvalue) in kwargs
-    Note that kwargs values may be any subclass of Record and it should be logged appropriately
-    """
-    ## Note that keys and values are guaranteed to be same order, since the dict is not
-    ## modified in between
-    ## Take care here, because its valid to set tags to None, so absence of kwargs["tags"] is not same as it being None
-    if isinstance(id,(list,tuple)) and len(id) == 0:
-        return # Nothing to change - saves callers checking for empty list always
-    if _skipNone:
-        kwargs = {k:v for k,v in kwargs.items() if v}  # Ignore non None
-    keys = kwargs.keys()
-    values = kwargs.values()
-    field_update = ", ".join("%s = ?" % k for k in keys)
-    where,ids = sqlpair("id",id)
-    updatesql = "UPDATE %s SET %s WHERE %s" % (table, field_update, where)
-    values = values + ids
-    # print "XXX@921", updatesql, values
-    curs = sqlSend(updatesql, values)
-    if curs.rowcount >0: pass
-    else:
-            raise Error("Failed to update - rowcount=%s sql=%s"
-                    % (rowcount, updatesql))
+	"""
+	Update Class[id][field]= newvalue for all (field, newvalue) in kwargs
+	Note that kwargs values may be any subclass of Record and it should be logged appropriately
+	"""
+	## Note that keys and values are guaranteed to be same order, since the dict is not
+	## modified in between
+	## Take care here, because its valid to set tags to None, so absence of kwargs["tags"] is not same as it being None
+	if isinstance(id,(list,tuple)) and len(id) == 0:
+		return # Nothing to change - saves callers checking for empty list always
+	if _skipNone:
+		kwargs = {k:v for k,v in kwargs.items() if v}  # Ignore non None
+	keys = kwargs.keys()
+	values = kwargs.values()
+	field_update = ", ".join("%s = ?" % k for k in keys)
+	where,ids = sqlpair("id",id)
+	updatesql = "UPDATE %s SET %s WHERE %s" % (table, field_update, where)
+	values = values + ids
+	# print "XXX@921", updatesql, values
+	curs = sqlSend(updatesql, values)
+	if curs.rowcount >0: pass
+	else:
+		raise Error("Failed to update - rowcount=%s sql=%s" % (rowcount, updatesql))
 
 def sqlSend(sql, parms=None):
-    """
-    Encapsulate most access to the sql server
-    Send a sql string to a server, with parms if supplied
+	"""
+	Encapsulate most access to the sql server
+	Send a sql string to a server, with parms if supplied
 
-    sql: sql statement that may contain usual "?" characters
-    parms[]: array or list of parameters to sql
-    ERR: IntegrityError (FOREIGN KEY constraint failed)
-    should catch database is locked errors and delay - may need to catch other errors but watch logs for them
-    """
-    db = get_db()
-    retrytime = 0.001       # Start with 1mS, might be far too short
-    while retrytime < 60: # Allows up to about 60 seconds of delay
-        try:
-            if parms is None:
-                curs = db.execute(sql)
-                db.commit()
-            else:  # parms supplied as array
-                curs = db.execute(sql, parms)
-                db.commit()
-        except sqlite3.OperationalError as e:
-            if 'database is locked' not in str(e):
-                break   # Drop out of loop and raise error
-            time.sleep(retrytime)
-            retrytime *= 2          # Try twice as long each iteration
-        except Exception as e:
-            break   # Drop out of loop and raise error
-        else: # No exception
-        	return curs
-    raise e
+	sql: sql statement that may contain usual "?" characters
+	parms[]: array or list of parameters to sql
+	ERR: IntegrityError (FOREIGN KEY constraint failed)
+	should catch database is locked errors and delay - may need to catch other errors but watch logs for them
+	"""
+	db = get_db()
+	retrytime = 0.001	   # Start with 1mS, might be far too short
+	while retrytime < 60: # Allows up to about 60 seconds of delay
+		try:
+			if parms is None:
+				curs = db.execute(sql)
+				db.commit()
+			else:  # parms supplied as array
+				curs = db.execute(sql, parms)
+				db.commit()
+		except sqlite3.OperationalError as e:
+			if 'database is locked' not in str(e):
+				break   # Drop out of loop and raise error
+			time.sleep(retrytime)
+			retrytime *= 2		  # Try twice as long each iteration
+		except Exception as e:
+			break   # Drop out of loop and raise error
+		else: # No exception
+			return curs
+	raise e
 
 def sqlFetch(sql, parms=None):
-    """
-    Encapsulate most access to the sql server
-    Send a sql string to a server, with parms if supplied
+	"""
+	Encapsulate most access to the sql server
+	Send a sql string to a server, with parms if supplied
 
-    sql: sql statement that may contain usual "?" characters
-    parms[]: array or list of parameters to sql
+	sql: sql statement that may contain usual "?" characters
+	parms[]: array or list of parameters to sql
 
 	returns array (possibly empty) of Rows (each of which behaves like a dict) as supplied by fetchall
 	"""
@@ -170,37 +169,96 @@ def sqlFetch(sql, parms=None):
 	return dd
 
 def find(table, nullbehavior, _skipNone=False, **kwargs):
-    """
-    Generic find
-    Searches a sql table, knows about "IS NULL" and tags.
-    """
-    # Preprocess vals
-    keys,val1 = zip(*[sqlpair(key, val) for key,val in kwargs.iteritems() if not (_skipNone and val is None)])
-    vals = flatten2d(val1)
-    sql = "SELECT * FROM %s WHERE %s" % (table, " AND ".join(keys))
-    return findAndCheckNull(sql,vals,"record matches fields", nullbehavior)
+	"""
+	Generic find
+	Searches a sql table, knows about "IS NULL" and tags.
+	"""
+	# Preprocess vals
+	keys,val1 = zip(*[sqlpair(key, val) for key,val in kwargs.iteritems() if not (_skipNone and val is None)])
+	vals = flatten2d(val1)
+	sql = "SELECT * FROM %s WHERE %s" % (table, " AND ".join(keys))
+	return findAndCheckNull(sql,vals,"record matches fields", nullbehavior)
 
 def findAndCheckNull(sql, parm, where, nullbehavior):
-    """
-    Do a SQL SELECT and
-    Handle the generic nullbehavior based on the length of the retrieved obj
-    sql is a SQL SELECT string to execute
-    parm is array of parameters to sql
-    where is the inner part of an error message
-    nullbehavior controls behavior if field doesn't point to anything
-    NULLERR Err 52; NULLNONE - return [ ]  -
-    ONLYONE says only ok if exactly one found, and return that err 63 if >1 or 52 if none
-    ONEORNONE says return obj if found, or None if not, err 63 if >1
-    FINDERR - Err 51 if found
-    All of these errors should be caught before the user sees them - 51 is (except in createNewAgent),
-    52 and 63 still need tracing
+	"""
+	Do a SQL SELECT and
+	Handle the generic nullbehavior based on the length of the retrieved obj
+	sql is a SQL SELECT string to execute
+	parm is array of parameters to sql
+	where is the inner part of an error message
+	nullbehavior controls behavior if field doesn't point to anything
+	NULLERR Err 52; NULLNONE - return [ ]  -
+	ONLYONE says only ok if exactly one found, and return that err 63 if >1 or 52 if none
+	ONEORNONE says return obj if found, or None if not, err 63 if >1
+	FINDERR - Err 51 if found
+	All of these errors should be caught before the user sees them - 51 is (except in createNewAgent),
+	52 and 63 still need tracing
 
-    ERR 51,52,63
+	ERR 51,52,63
 
-    Subclassed by Deal to handle subtypes
-    """
-    rr = sqlFetch(sql,parm)
-    return checkNull(rr, where, nullbehavior)
+	Subclassed by Deal to handle subtypes
+	"""
+	dd = sqlFetch(sql,parm)
+	return checkNull(dd, where, nullbehavior)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+### Functions ###
+
+def printall(): # Prints the entire database to the console window
+	db = get_db()
+	cursor = db.cursor()
+	cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+	tables = cursor.fetchall()
+	print '\n'*2
+	print '='*100
+	for t in tables:
+		name = t[0]
+		if name != 'sqlite_sequence':
+			print 'Name: ', name
+			cursor.execute("SELECT * FROM "+name)
+			print 'Columns: ', [description[0] for description in cursor.description]
+			print 'Data: ', cursor.fetchall()
+		print '-'*100
+	return 
+
+def getCivicData(address):
+	# https://developers.google.com/civic-information/docs/v2/representatives/representativeInfoByAddress
+	# https://developers.google.com/civic-information/docs/v2/representatives#resource
+	# https://developers.google.com/resources/api-libraries/documentation/civicinfo/v2/python/latest/civicinfo_v2.representatives.html#representativeInfoByAddress
+	civicapikey = 'AIzaSyCjQMFoXcL1iO6eX_ixCZ1uDLJeqUttQMU' # from https://console.developers.google.com/apis/library?project=one-call-today-1480143256556
+	url = 'https://www.googleapis.com/civicinfo/v2/representatives?address='+str(address)+'&alt=json&key='+civicapikey
+	req = Request(url)
+	try:
+		response = urlopen(req)
+	except URLError as e:
+		if hasattr(e, 'reason'):
+			print("\nERROR:\n" + e.reason + "\n")
+		elif hasattr(e, 'code'):
+			print("\nERROR: \nThe server couldn't fulfill the request. \n" + e.code + "\n")
+		return None  # Error
+	else: # everything is fine
+		return json.loads(response.read().decode('utf8'))
+
+def listTargets(campaign, caller):
+	"""
+	Takes in a campaign and address, and suggests who should be called with phone numbers in the form:
+	{u'Joe Manchin III': [u'(202) 224-3954'], u'Shelley Moore Capito': [u'(202) 224-6472', u'(304) 347-5372', u'(304) 262-9285']}
+	"""
+	cd = getCivicData(caller['zipcode'])
+	officialsenum = [(k['officialIndices'],k['name']) for k in cd['offices'] if ('roles' in k) and ([i for i in k['roles'] if i in campaign['offices']])]
+	targets = []
+	for indices,office in officialsenum:
+		for i in indices:
+			if ('phones' in cd['officials'][i]) and (cd['officials'][i]['party'] in campaign['targetparties']):
+				targets = targets+[{'name':cd['officials'][i]['name'], 'phones':cd['officials'][i]['phones'], 'office':office}]
+	return targets
+
+def listCampaigns(caller):
+	"""
+	Takes in a caller, returns a list of campaign dicts that the caller has NOT yet called about, but which are ongoing right now.
+	"""
+	calls = [call['campaignid'] for call in find('call', NULLNONE, callerid=caller['id'])]
+	return [camp for camp in find('campaign', NULLNONE, id='%%', startdate='< '+str(time()), enddate='> '+str(time())) if camp['id'] not in calls]
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ### URL Calls ###
@@ -259,22 +317,22 @@ def findcallers():
 
 @app.route("/callpaul", methods=['GET', 'POST'])
 def hello_monkey():
-    """Respond to incoming requests."""
-    resp = twilio.twiml.Response()
-    resp.say("It's time to call Jona")
-    # Dial (310) 555-1212 - connect that number to the incoming caller.
-    resp.dial("+16178432883")
-    return str(resp)
+	"""Respond to incoming requests."""
+	resp = twilio.twiml.Response()
+	resp.say("It's time to call Jona")
+	# Dial (310) 555-1212 - connect that number to the incoming caller.
+	resp.dial("+16178432883")
+	return str(resp)
 
 @app.route("/textseth", methods=['GET'])
 def text_seth():
 	"Send a text message to seth"
 	client = TwilioRestClient(account_sid, auth_token)
 	message = client.messages.create(to="+16177107496", from_="+16179256394",
-                                     body="Hello there!")
+									 body="Hello there!")
 	return "success"
 
 
 
 if __name__ == "__main__":
-    app.run(debug=True) # Set debug=True so that saving this file automatically restarts the flask app
+	app.run(debug=True) # Set debug=True so that saving this file automatically restarts the flask app
