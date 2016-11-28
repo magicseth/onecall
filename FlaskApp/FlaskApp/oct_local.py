7d6f7279610a8bf1dfe68c2fe37fbf2ab4321602
Seth Raphael <seth@bu.mp>
2016-11-27 20:15:01 -0800

    deleting opt_local

---
+++ FlaskApp/FlaskApp/oct_local.py
@@ -0,0 +1,4 @@
+### Local Variables (gitignored)
+
+dir_path = '/Users/raphael/onecall/FlaskApp/FlaskApp'
+script_path = '/Users/raphael/onecall/scripts' # XXX This should really be the same as the dir_path, but I couldn't figure out how to make the createdb.sh put it in the dir_path
