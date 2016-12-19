317fc0079746f4115c82d2324984cf4224692718
Jona Raphael <jona@lumeter.net>
2016-12-19 02:50:06 -0800

    holy shit it worked

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -844,10 +844,6 @@ def receive_sms():
 	senderid = sender['id'] if sender else insertR('caller',[None, number, INACTIVE, PREFUNREG, WEEKDAY])
 	insertR('sms',[None, now, senderid, None, message_body, SMSIN])
 	resp = smsdispatch(number, message_body)
-	app.logger.error('?><?><?><?><?><?><?><?><?><?><?><?><')
-	app.logger.error(str(resp))
-	app.logger.error(ET.fromstring(str(resp)).findall('.//Response/Message/Body'))
-	app.logger.error(ET.fromstring(str(resp)).findall('.//Body'))
 	for body in ET.fromstring(str(resp)).findall('.//Body'):
 		insertR('sms',[None, now, senderid, None, body.text, SMSOUT])	
 	app.logger.info(resp)
